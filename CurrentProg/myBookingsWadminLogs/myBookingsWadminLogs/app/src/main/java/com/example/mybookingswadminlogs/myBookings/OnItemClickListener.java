28c1ec5414ab91599f6d1d1ab0a3637e44184786
bruhrian <brianchuayx@gmail.com>
2026-03-19 19:46:49 +0800

    Delete CurrentProg directory

--- CurrentProg/myBookingsWadminLogs/myBookingsWadminLogs/app/src/main/java/com/example/mybookingswadminlogs/myBookings/OnItemClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.mybookingswadminlogs.myBookings;
-
-public interface OnItemClickListener {
-    void onDeleteClick(int position);
-}
--- CurrentProg/myBookingsWadminLogs/myBookingsWadminLogs/app/src/main/java/com/example/mybookingswadminlogs/myBookings/adminlogs/OnLogItemLongClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.mybookingswadminlogs.myBookings.adminlogs;
-
-public interface OnLogItemLongClickListener {
-    void onLogItemLongClicked(int logId, String organization, int position);
-}
