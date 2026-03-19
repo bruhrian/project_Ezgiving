5bb841b1c75b665130736ae465ef053ea8836fcf
bruhrian <brianchuayx@gmail.com>
2026-03-19 19:46:39 +0800

    Delete TempSepFiles directory

--- TempSepFiles/RecyclerView w no searchbar plus havent changed to SQLite/ReSnek/app/src/main/java/com/example/resnek/RecycleViewInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.resnek;
-
-public interface RecycleViewInterface {
-    // can pass in any mtd uw, make sure implement all of item in main
-    void onItemClick(int pos);
-    void onItemLongClick(int pos);
-}
